8ce22fdc5de8a5ed2693bd262764aa043ff2ac5d
Aleksandr Afonin <focussashka161@gmail.com>
2025-11-14 20:42:57 +0300

    initial commit

---
+++ LibraryApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LibraryApp
+//
+//  Created by Aleksandr on 11/14/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+	var timestamp: Date
+	
+	init(timestamp: Date) {
+		self.timestamp = timestamp
+	}
+}
